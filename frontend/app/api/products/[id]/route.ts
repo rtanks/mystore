0614e5560f3f1af4b9b3eb9a1e4e8c0c7b4650be
rs <r.sassassin200180@gmail.com>
2025-10-30 19:29:01 +0330

    Initial commit - monorepo Next + Nest

--- frontend/app/api/products/[id]/route.ts
+++
@@ -1,9 +0,0 @@
-export const GET = async (request: Request, context: {
-    params: {
-        id: string,
-    }
-}) => {
-    const {id} = await context.params;
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products/${id}`)
-    return response;
-}
--- frontend/app/api/products/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const GET = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products`)
-    return response;
-}
